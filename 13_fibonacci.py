a34d0454de73d3a54b26faacb0fdc6e3fa4d8a3d
Matt <allen.mattp@gmail.com>
2021-02-17 14:59:55 -0800

    Added file 13

---
+++ 13_fibonacci.py
@@ -0,0 +1,31 @@
+"""
+Write a program that asks the user how many Fibonnaci numbers to generate and then generates them.
+"""
+
+
+def main():
+    num = int(input("How many Fibonnaci numbers should we generate? "))
+
+    # track two variables: the current and previous number
+    current = 1
+    previous = 1
+    # store numbers in a list
+    list = [0]
+    for i in range(num):
+        # First number in sequence (after 0) is 1
+        if not i:
+            list.append(current)
+        # Second number in sequence is also 1
+        elif i == 1:
+            list.append(current)
+        else:
+            # find current value by adding previous value
+            current += previous
+            # set value for previous so it's ready for next iteration
+            previous = current - previous
+            list.append(current)
+    print("All numbers:", list)
+    print("Last number:", list[-1])
+
+if __name__ == '__main__':
+    main()
