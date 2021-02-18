c9c071912ccc69fa3fd090fd0b5335e7b52fc850
Matt <allen.mattp@gmail.com>
2021-02-17 16:03:03 -0800

    Added file 15

--- 15_reverse_word_order.py
+++ 15_reverse_word_order.py
@@ -5,7 +5,9 @@ Print back to the user the same string, except with the words in backwards order
 
 def main():
     def reverse():
+        # split string on whitespace into string_list, starting from last word
         string_list = str(input("Enter text: ")).split()[::-1]
+        # join our string_list with words separated by a space and return
         return " ".join(string_list)
 
     print(reverse())
